216b3fdfa3ed262b63df8a5cd3b2f3819e63a254
Qinglin <qy2211@columbia.edu>
2019-12-08 22:06:45 -0500

    new version with dynamodb

--- app.js
+++ app.js
@@ -1,62 +1,143 @@
-//jshint esversion:6
+// Include the cluster module
+var cluster = require('cluster');
 
-const express = require("express");
-const bodyParser = require("body-parser");
+// Code to run if we're in the master process
+if (cluster.isMaster) {
 
-const app = express();
+    // Count the machine's CPUs
+    var cpuCount = require('os').cpus().length;
 
-let items =[];
-let workItems = [];
-app.set('view engine', 'ejs');
+    // Create a worker for each CPU
+    for (var i = 0; i < cpuCount; i += 1) {
+        cluster.fork();
+    }
 
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(express.static("public"));
+    // Listen for terminating workers
+    cluster.on('exit', function (worker) {
 
-app.get("/", function(req, res){
+        // Replace the terminated workers
+        console.log('Worker ' + worker.id + ' died :(');
+        cluster.fork();
 
-  let today = new Date();
+    });
+} else {
+  var AWS = require('aws-sdk');
+  const express = require("express");
+  const bodyParser = require("body-parser");
+  const date = require(__dirname + "/date.js");
 
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month:"long"
-  };
+  AWS.config.region = process.env.REGION
+  var ddb = new AWS.DynamoDB();
+  var ddbTable =  process.env.todoApp;
+  var app = express();
 
-  let day = today.toLocaleDateString("en-US", options);
+  app.set('view engine', 'ejs');
+  app.set('views', __dirname + '/views');
+  app.use(bodyParser.urlencoded({extended: true}))
+  app.use(express.static("public"));
 
-  res.render("list", {
-    listTitle: day,
-    newListItems: items
+
+  app.get("/", function(req, res){
+    let items =[];
+    let day = date.getDate()
+
+    var params = {
+      ProjectionExpression: 'name, tag, day',
+      TableName: ddbTable
+    };
+
+    ddb.scan(params, function(err, data) {
+      if (err) {
+        console.log("Error", err);
+      } else {
+        console.log("Success", data.Items);
+        data.Items.forEach(function(task) {
+          items.push(task.name);
+        });
+      }
+    });
+
+    res.render("list", {
+      listTitle: day,
+      newListItems: items
+    });
+
   });
 
-});
+  app.post('/', function(req,res){
+    let name = req.body.newItem;
+    let day = date.getDate()
+    var item = {
+            'name': {'S': req.body.newItem},
+            'tag': {'S': req.body.list}
+            'day': {'S': day}
+        };
 
-app.post('/', function(req,res){
-  let item = req.body.newItem;
+    var params = {
+      TableName: ddbTable,
+      Item: item,
+      'Expected': { name: { Exists: false } }
+    };
 
-  if(req.body.list==="Work"){
-    workItems.push(item);
-    res.redirect("/work");
-  }else{
-    items.push(item);
-    res.redirect("/");
+    ddb.putItem(params, function(err, data) {
+        if (err) {
+            var returnStatus = 500;
+            if (err.code === 'ConditionalCheckFailedException') {
+                returnStatus = 409;
+            }
+            res.status(returnStatus).end();
+            console.log('DDB Error: ' + err);
+        }else{
+          console.log('Success adding new task');
+        }
+      }
+    });
+
+    if(req.body.list==="Work"){
+      res.redirect("/work");
+    }else{
+      res.redirect("/");
+    }
+  });
+
+
+  app.get("/work",function(req, res){
+    let workItems = [];
+    var params = {
+      TableName: ddbTable,
+      ProjectionExpression: 'name, tag, day',
+      KeyConditionExpression:'tag = :t',
+      ExpressionAttributeNames:{
+        ':t': 'work'
+      }
+    };
+
+    ddb.query(params, function(err, data) {
+      if (err) {
+        console.log("Error", err);
+      } else {
+        console.log("Success", data.Items);
+        data.Items.forEach(function(task) {
+          workItems.push(task.name);
+        });
+      }
+    });
+    res.render("list", {
+      listTitle: "Work",
+      newListItems: workItems
+    });
+  })
+
+  // app.post('/work', function(req,res){
+  //   let item = req.body.newItem;
+  //   workItems.push(item);
+  //   res.redirect("/work");
+  // });
+
+
+  var port = process.env.PORT || 3000;
+
+  var server = app.listen(port, function () {
+      console.log('Server running at http://127.0.0.1:' + port + '/');
+    });
   }
-});
-
-app.get("/work",function(req, res){
-  res.render("list", {
-    listTitle: "Work List",
-    newListItems: workItems
-  });
-})
-
-// app.post('/work', function(req,res){
-//   let item = req.body.newItem;
-//   workItems.push(item);
-//   res.redirect("/work");
-// });
-
-
-app.listen(3000, function(){
-  console.log("Server started on port 3000.");
-});
